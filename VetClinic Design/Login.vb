6e43e636e38fd4c81be510040cd8c4df2a13b202
juzt-1n <justincosme01@gmail.com>
2025-05-10 21:30:05 +0800

    Update Login.vb

--- VetClinic Design/Login.vb
+++ VetClinic Design/Login.vb
@@ -13,28 +13,15 @@
     End Sub
 
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
-        Dim iExit As DialogResult
-
-        iExit = MessageBox.Show("Are you sure you want to exit?", "Veterinary Clinic System", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
-
-        If iExit = DialogResult.Yes Then
+        
             Me.Close()
         End If
 
-    End Sub
+   
 
-    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Try
-            openCon()
-            MsgBox("Connected!")
-            con.Close()
-        Catch ex As Exception
-            MessageBox.Show(ex.Message, "Error Occurred!", MessageBoxButtons.OK, MessageBoxIcon.Information)
-            Me.Close()
-        End Try
+    
 
 
-    End Sub
 
 
 End Class
